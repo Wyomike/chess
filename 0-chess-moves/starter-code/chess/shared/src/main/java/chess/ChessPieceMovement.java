255ca7a6166d056f81f2b2fcedd5d0e46920d585
Wyomike <wyomike2020@gmail.com>
2024-01-13 21:25:27 -0700

    Replaced buggy test stuff

--- 0-chess-moves/starter-code/chess/shared/src/main/java/chess/ChessPieceMovement.java
+++ 0-chess-moves/starter-code/chess/shared/src/main/java/chess/ChessPieceMovement.java
@@ -3,5 +3,5 @@ package chess;
 import java.util.Collection;
 //MAKE THIS A TEMPLATE CLASS!
 interface ChessPieceMovement {
-    public Collection<ChessMove> validMoves();
+    public Collection<ChessMove> validMoves(ChessPosition start);
 }
--- 0-chess-moves/starter-code/chess/shared/src/main/java/chess/KingMovement.java
+++ 0-chess-moves/starter-code/chess/shared/src/main/java/chess/KingMovement.java
@@ -3,7 +3,8 @@ package chess;
 import java.util.Collection;
 
 public class KingMovement implements ChessPieceMovement {
-    public Collection<ChessMove> validMoves() {
+    public Collection<ChessMove> validMoves(ChessPosition start) {
+        // Make chessmoves that can go +- 1 in each direction with promotion type null.
         return new Collection<ChessMove>();
     }
 }
